0c95eab576c05d222ea36473bbcb7b7abe183cb2
furqonaugust17 <furqonaugustseventeenth@gmail.com>
2024-11-09 17:52:35 +0700

    first upload

---
+++ routes/web.php
@@ -0,0 +1,10 @@
+<?php
+
+use App\Http\Resources\User;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::apiResource('/users', User::class);
